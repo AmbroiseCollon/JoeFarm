4b48a2825b14337db31063ace94af68993815218
AmbroiseCollon <ambroise.collon@gmail.com>
2017-02-14 12:56:21 +0100

    Initial Commit - Chapter 2

---
+++ JoeFarm.playground/Contents.swift
@@ -0,0 +1,8 @@
+// Le prix du voyage aux Caraïbes de Joe
+let price = 1499
+
+// Le bénéfice quotidien de Joe avec sa ferme
+let dailyBenefit = 10
+
+// Le nombre de jours pendant lesquelles Joe doit économiser
+let result = price / dailyBenefit
