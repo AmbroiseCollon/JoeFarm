e69661de0b429841830b8c5ef37e62e32c9e7c9f
AmbroiseCollon <ambroise.collon@gmail.com>
2017-02-15 10:30:58 +0100

    Chapter 5 - Conditions

--- JoeFarm.playground/Contents.swift
+++ JoeFarm.playground/Contents.swift
@@ -1,10 +1,19 @@
 var money = 0.0
 
-for _ in 1...30 {
-    // Joe nourrit les vaches
+for day in 1...30 {
+    // Joe nourrit les vaches tous les jours
     money -= 4
-    // Joe vends son lait
-    money += 30 * 0.50
+
+    if day == 1 {
+        // Joe moissonne
+        money += 100 * 0.30
+    } else if day == 10 || day == 20 {
+        // Joe tonds les moutons
+        money += 30 * 1
+    } else {
+        // Joe vends son lait
+        money += 30 * 0.50
+    }
 }
 
 print("\(money) €")
