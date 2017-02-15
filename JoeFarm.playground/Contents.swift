a6f571568a128381d9664343e642eacbb701d726
AmbroiseCollon <ambroise.collon@gmail.com>
2017-02-15 19:35:47 +0100

    Chapter 6 - While Loop

--- JoeFarm.playground/Contents.swift
+++ JoeFarm.playground/Contents.swift
@@ -1,19 +1,29 @@
+// Le prix du voyage
+var price = 1499.0
+
+// Le nombre de jours pendant lesquels Joe doit économiser
+var numberOfDay = 0
+
+// L'argent de Joe
 var money = 0.0
 
-for day in 1...30 {
+while money < price {
     // Joe nourrit les vaches tous les jours
     money -= 4
 
-    if day == 1 {
+    if numberOfDay % 30 == 1 {
         // Joe moissonne
         money += 100 * 0.30
-    } else if day == 10 || day == 20 {
+    } else if numberOfDay % 30 == 10 || numberOfDay % 30 == 20 {
         // Joe tonds les moutons
         money += 30 * 1
     } else {
         // Joe vends son lait
         money += 30 * 0.50
     }
+
+    // On passe au jour suivant
+    numberOfDay += 1
 }
 
-print("\(money) €")
+print("Il aura fallu \(numberOfDay) jours à Joe pour économiser \(money) €")
