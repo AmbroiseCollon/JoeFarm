7b31d7941e6ed74ceed3aaadeec078b2e5403cbe
AmbroiseCollon <ambroise.collon@gmail.com>
2017-02-27 12:40:47 +0000

    Chapter 8 & 9 - Dictionaries and optionals

--- JoeFarm.playground/Contents.swift
+++ JoeFarm.playground/Contents.swift
@@ -8,7 +8,7 @@ var money = 0.0
 var numberOfDay = 0
 
 // La grange de Joe : [lait, blé, laine]
-var barn = [0, 0, 0]
+var barn = ["milk": 0, "wheat": 0, "wool": 0]
 
 while money < price {
     // Joe nourrit les vaches tous les jours
@@ -16,35 +16,36 @@ while money < price {
 
     // On calcule la taille de la grange
     var barnSize = 0
-    for goods in barn {
-        barnSize += goods
+    for (goods, count) in barn {
+        barnSize += count
     }
 
     if barnSize >= 500 {
         // On vends !
-        money += Double(barn[0]) * 0.50 // Le lait
-        money += Double(barn[1]) * 0.30 // Le blé
-        money += Double(barn[2]) * 1 // La laine
+        money += Double(barn["milk"]!) * 0.50
+        money += Double(barn["wheat"]!) * 0.30
+        money += Double(barn["wool"]!) * 1
 
         // On vide la grange
-        barn = [0, 0, 0]
+        barn = ["milk": 0, "wheat": 0, "wool": 0]
     } else {
         // C'est une journée normale
 
         if numberOfDay % 30 == 1 {
             // Joe moissonne
-            barn[1] += 100
+            barn["wheat"]! += 100
         } else if numberOfDay % 30 == 10 || numberOfDay % 30 == 20 {
             // Joe tond les moutons
-            barn[2] += 30
+            barn["wool"]! += 30
         } else {
             // Joe trait les vaches
-            barn[0] += 30
+            barn["milk"]! += 30
         }
     }
 
     // On passe au jour suivant
     numberOfDay += 1
 }
+
 
 print("Il aura fallu \(numberOfDay) jours à Joe pour économiser \(money) €")
